c9e60d37108783502b80f5ce9b1150bbe9127f23
renukajalluri <92221204+renukajalluri@users.noreply.github.com>
2022-04-07 12:38:13 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 //require the library
 const mongoose = require("mongoose");
 
-const DB = 'mongodb+srv://Signup:123@cluster0.nnrbm.mongodb.net/todolist?retryWrites=true&w=majority'
+const DB = 'mongodb+srv://Signup:123@cluster0.nnrbm.mongodb.net/todolist?retryWrites=true&w=majority';
 
 
 
@@ -12,8 +12,5 @@ mongoose.connect(DB,{
 }).then(()=>{
     console.log('connection successful')
 }).catch((err)=>{
-    
-    console.log('no connection'),
-                
-console.log(err),              
+   console.log(err)            
 })
